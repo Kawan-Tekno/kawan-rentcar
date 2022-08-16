9301c474e92058a7f6c8f9168c83d6d7405b6dd2
inubayuaji <inubayuaji@gmail.com>
2022-08-16 14:22:05 +0700

    add bootstrap 5 and fix react

--- resources/js/components/Card.jsx
+++ resources/js/components/Card.jsx
@@ -1,5 +1,5 @@
 import React from "react";
 
 export default function Card(props) {
-  return <card>{props.name}</card>
+  return <div>{props.name}</div>
 }
--- resources/js/components/Navabar.jsx
+++ resources/js/components/Navabar.jsx
@@ -2,10 +2,8 @@ import React from "react";
 
 export default function Navbar() {
   return (
-    <div className="flex flex-wrap w-full">
-      <div className="container">
-        <h1 className="text-lg font-bold">Haloo</h1>  
-      </div>      
+    <div className="container">
+      <h1>Haloo</h1>
     </div>
   );
 }
